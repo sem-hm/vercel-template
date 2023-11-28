f1188908a9b645bb4ec83238910aab830b6a8728
Ubaldo Luna <100547074+UbaldoRdz98@users.noreply.github.com>
2023-11-28 01:39:31 +0000

    New view changes

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,10 @@
 # example/urls.py
 from django.urls import path
 
-from example.views import index
-
+from .views import home, index, indexImage
 
 urlpatterns = [
     path('', index),
+    path('home/', home, name='home'),
+    path('indexImage/', indexImage, name='indexImage')
 ]
--- example/views.py
+++ example/views.py
@@ -7,3 +7,11 @@ from django.http import HttpResponse
 def index(request):
     now = datetime.now()
     return render(request,'example/index.html', {"current_time":now})
+
+def home(request):
+    now = datetime.now()
+    return render(request,'example/home.html', {"current_time":now})
+
+def indexImage(request):
+    now = datetime.now()
+    return render(request,'example/images.html', {"current_time":now})
